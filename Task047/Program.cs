21fcc7f4ae43289b3b5373d5d89f817fc75f8ade
davydova <natalya-davydova-86@mail.ru>
2023-06-01 19:37:19 +0300

    Created Task 047

---
+++ Task047/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив размером m x n, заполненный случайными
+// вещественными числами.
+// m = 3, n = 4
+
+Console.Write("Введтие количесвто строк массива (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] randomArray = new double[m,n];
+
+void mas(int m, int n)
+{
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
+    {
+    Console.WriteLine();
+    for (j = 0; j < n; j++)
+        {
+            randomArray[i,j] = rand.NextDouble();
+            Console.Write($"{randomArray[i, j]:F2}");
+        }
+
+    }
+
+}
+
+mas(m,n);
