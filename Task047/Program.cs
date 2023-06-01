da8660af5111232b36190e96d030ecc2c5d907d8
davydova <natalya-davydova-86@mail.ru>
2023-06-01 19:39:42 +0300

    Changed  Task 047

--- Task047/Program.cs
+++ Task047/Program.cs
@@ -10,12 +10,12 @@ double[,] randomArray = new double[m,n];
 
 void mas(int m, int n)
 {
-int i,j;
-Random rand = new Random();
-for (i = 0; i < m; i++)
+    int i,j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
     {
-    Console.WriteLine();
-    for (j = 0; j < n; j++)
+        Console.WriteLine();
+        for (j = 0; j < n; j++)
         {
             randomArray[i,j] = rand.NextDouble();
             Console.Write($"{randomArray[i, j]:F2}");
@@ -24,5 +24,4 @@ for (i = 0; i < m; i++)
     }
 
 }
-
 mas(m,n);
