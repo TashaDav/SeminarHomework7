53a193ca2ee71daef99b665a325d85e4b0431fdc
davydova <natalya-davydova-86@mail.ru>
2023-06-02 10:49:53 +0300

    Changed Task 050 again

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -6,38 +6,7 @@
 // 8 4 2 4
 // 17 -> такого элемента нет
 
-/*
-Console.WriteLine("Введите размеры массива");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
- 
-for (int i = 0; i < array.GetLength(0); i++) 
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
-}
- 
-for (int i = 0; i < array.GetLength(0); i++) 
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-        Console.Write(array[i,j] + "\t  ");
-        Console.WriteLine();
-}
- 
- Console.WriteLine("Введите координаты");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a>m && b>n)
- Console.WriteLine("такого числа нет");
- else
- {
- object c = array.GetValue(a,b);
- Console.WriteLine(c);
- }
- */
-
- Console.Write("Введите количесвто строк массива (m): ");
+Console.Write("Введите количесвто строк массива (m): ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количесвто столбцов массива (n): ");
 int n = Convert.ToInt32(Console.ReadLine());
